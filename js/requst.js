c853fc8ad9534beb44b92175ec0f889082a3db7e
pkpinnersNots <122718850@qq.com>
2019-05-23 17:35:38 +0800

    frist

---
+++ js/requst.js
@@ -0,0 +1,15 @@
+// 请求api接口数据
+
+// 音乐排行榜接口：https://api.apiopen.top/musicRankingsDetails?type=1
+// 视频分类推荐接口:https://api.apiopen.top/videoCategory
+// 新闻接口：https://www.apiopen.top/journalismApi
+// 音乐电台接口：https://api.apiopen.top/musicBroadcastingDetails?channelname=public_tuijian_spring
+
+<script type="text/javascript">
+
+    $(function (params) {
+        $('.text').on('click', function(){
+            var text
+        })
+    })
+</script>
